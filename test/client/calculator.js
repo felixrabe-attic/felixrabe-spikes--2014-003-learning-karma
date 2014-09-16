e045cafffa933b09fcdd530d16274242fc81d2c1
Felix Rabe <felix@rabe.io>
2014-09-16 14:18:36 +0200

    Describe test correctly

--- test/client/calculator.js
+++ test/client/calculator.js
@@ -2,9 +2,9 @@
 
 var assert = chai.assert;
 
-describe('Something', function() {
+describe('Calculator', function() {
 
-  it('should run', function() {
+  it('should add', function() {
     assert.equal(5, calculator.add(2, 3));
     assert.equal(-2, calculator.add(-1, -1));
   });
