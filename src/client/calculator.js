7fb9bbee126c262c3030d573d1ed745b910742d3
Felix Rabe <felix@rabe.io>
2014-09-16 14:06:53 +0200

    Spike

---
+++ src/client/calculator.js
@@ -0,0 +1,9 @@
+(function() {
+  var calculator = window.calculator = (function() {
+    return {
+      add: function(a, b) {
+        return a + b;
+      }
+    };
+  })();
+})();
---
+++ test/client/calculator.js
@@ -0,0 +1,12 @@
+// var assert = require('chai').assert;
+
+var assert = chai.assert;
+
+describe('Something', function() {
+
+  it('should run', function() {
+    assert.equal(5, calculator.add(2, 3));
+    assert.equal(-2, calculator.add(-1, -1));
+  });
+
+});
